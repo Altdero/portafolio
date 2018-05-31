95b27374982e5c132acfb3e16d6e8acb9e51ac76
Ivan Leon <hannibal.chaos@gmail.com>
2018-05-31 10:25:41 -0500

    Quite console.log de la data del Json

--- src/app/providers/informacion.service.ts
+++ src/app/providers/informacion.service.ts
@@ -12,7 +12,6 @@ export class InformacionService {
   constructor( public http: Http ) {
 
     http.get('assets/data/info.pagina.json').subscribe( data => {
-      console.log(data.json());
       this.info = data.json();
       this.cargando = true;
     } );
