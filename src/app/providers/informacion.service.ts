8772b24be457744ba52c9f4fa83b9a84068b96a4
Ivan Leon <hannibal.chaos@gmail.com>
2018-06-06 10:23:37 -0500

    Servicio de Firebase listo

--- src/app/providers/informacion.service.ts
+++ src/app/providers/informacion.service.ts
@@ -6,15 +6,37 @@ import { Http } from '@angular/http';
 })
 export class InformacionService {
 
-  info: any = {};
-  cargando = false;
+  public info: any = {};
+  public infoCargada = false;
+  public equipo: any[] = [];
+  // public equipo: Array<any> = [];
+  public sobreNosotrosCargada = false;
 
   constructor( public http: Http ) {
 
-    http.get('assets/data/info.pagina.json').subscribe( data => {
+    this.cargaInfo();
+    this.cargaSobreNosotros();
+
+  }
+
+  public cargaInfo() {
+
+    this.http.get('assets/data/info.pagina.json').subscribe( data => {
+      this.infoCargada = true;
       this.info = data.json();
-      this.cargando = true;
+      console.log(this.info);
     } );
 
   }
+
+  public cargaSobreNosotros() {
+
+    this.http.get('https://portafolio-b2fcc.firebaseio.com/equipo.json').subscribe( data => {
+      this.sobreNosotrosCargada = true;
+      this.equipo = data.json();
+      console.log(this.equipo);
+    } );
+
+  }
+
 }
